854e814a43e3db816b0196d58b318e292536352f
Felipe Barcelos <felipebarcelospro@iMac-de-Felipe.local>
2025-05-13 23:40:30 -0700

    feat: update

--- packages/modules/src/domain/usecases/transaction/create-transaction.ts
+++ packages/modules/src/domain/usecases/transaction/create-transaction.ts
@@ -1,8 +1,8 @@
-import { IExchangeProvider } from '../../../interfaces/providers/exchange'
-import { IFiatProvider } from '../../../interfaces/providers/fiat'
-import { ITransactionRepository } from '../../../interfaces/repositories/transaction'
-import { IUserRepository } from '../../../interfaces/repositories/user'
-import { Transaction } from '../../entities/Transaction'
+import { IExchangeProvider } from "../../../interfaces/providers/exchange";
+import { IFiatProvider } from "../../../interfaces/providers/fiat";
+import { ITransactionRepository } from "../../../interfaces/repositories/transaction";
+import { IUserRepository } from "../../../interfaces/repositories/user";
+import { Transaction } from "../../entities/Transaction";
 
 export class CreateTransactionUseCase {
   constructor(
@@ -18,62 +18,62 @@ export class CreateTransactionUseCase {
     amount,
     address,
   }: {
-    userId: string
-    type: 'buy' | 'sell'
-    amount: number
-    address: string
+    userId: string;
+    type: "buy" | "sell" | "pay";
+    amount: number;
+    address: string;
   }): Promise<Transaction> {
-    const EXPIRE_IN_15_MINUTES = 15 * 60
+    const EXPIRE_IN_15_MINUTES = 15 * 60;
 
-    const user = await this.userRepository.getById(userId)
-    console.log('User fetched:', user)
+    const user = await this.userRepository.getById(userId);
+    console.log("User fetched:", user);
 
     if (!user) {
-      throw new Error('User not found')
+      throw new Error("User not found");
     }
 
-    if (type === 'sell') {
+    if (type === "sell") {
       const estimate = await this.exchangeProvider.getEstimateForExchange({
         amount,
-        from: 'ada',
-        to: 'usdttrc20',
-      })
-      console.log("Estimate for 'sell':", estimate)
+        from: "ada",
+        to: "usdttrc20",
+      });
+      console.log("Estimate for 'sell':", estimate);
 
       const fiatExchange = await this.fiatExchangeProvider.createExchange({
         address,
         amount: estimate.outAmount,
-        type: 'sell',
+        type: "sell",
         user: {
           email: user.email,
-          document: user.settings.kyc.data.document?.replace(/\D/g, '') || '',
-          phone: user.settings.contact.phone?.replace(/\D/g, '') || '',
-          country: user.settings.kyc.data.address.country || 'BR',
+          document: user.settings.kyc.data.document?.replace(/\D/g, "") || "",
+          phone: user.settings.contact.phone?.replace(/\D/g, "") || "",
+          country: user.settings.kyc.data.address.country || "BR",
           address: {
             street: user.settings.kyc.data.address.street as string,
             number: user.settings.kyc.data.address.number as string,
             zipCode: user.settings.kyc.data.address.zipCode as string,
           },
         },
-      })
-      console.log("Fiat exchange created for 'sell':", fiatExchange)
+      });
+      console.log("Fiat exchange created for 'sell':", fiatExchange);
 
       if (!fiatExchange.address) {
-        throw new Error('Failed to create fiat exchange')
+        throw new Error("Failed to create fiat exchange");
       }
 
       const exchange = await this.exchangeProvider.createExchange({
         addressRecipient: fiatExchange.address,
         amount: fiatExchange.estimate.amountInCrypto,
         rateId: estimate.rateId,
-      })
-      console.log("Crypto exchange created for 'sell':", exchange)
+      });
+      console.log("Crypto exchange created for 'sell':", exchange);
 
       return this.transactionRepository.create({
         userId,
-        type: 'WITHDRAW',
-        fromCurrency: 'ADA',
-        toCurrency: 'BRL',
+        type: "WITHDRAW",
+        fromCurrency: "ADA",
+        toCurrency: "BRL",
         exchangeId: exchange.id,
         exchangeAddress: exchange.exchangeAddress,
         paymentId: fiatExchange.id,
@@ -82,56 +82,56 @@ export class CreateTransactionUseCase {
         fromAmount: amount,
         toAmount: Number(fiatExchange.estimate.sendInFiat),
         expiresAt: new Date(new Date().getTime() + EXPIRE_IN_15_MINUTES * 1000),
-      })
+      });
     }
 
     const fiatEstimate = await this.fiatExchangeProvider.getEstimateForExchange(
       {
-        type: 'sell',
+        type: "sell",
         amount,
       },
-    )
-    console.log("Fiat estimate for 'buy':", fiatEstimate)
+    );
+    console.log("Fiat estimate for 'buy':", fiatEstimate);
 
     const exchangeEstimate = await this.exchangeProvider.getEstimateForExchange(
       {
         amount: fiatEstimate.sendInCrypto,
-        from: 'usdttrc20',
-        to: 'ada',
+        from: "usdttrc20",
+        to: "ada",
       },
-    )
-    console.log("Exchange estimate for 'buy':", exchangeEstimate)
+    );
+    console.log("Exchange estimate for 'buy':", exchangeEstimate);
 
     const exchange = await this.exchangeProvider.createExchange({
       addressRecipient: address,
       amount: fiatEstimate.sendInCrypto,
       rateId: exchangeEstimate.rateId,
-    })
-    console.log("Crypto exchange created for 'buy':", exchange)
+    });
+    console.log("Crypto exchange created for 'buy':", exchange);
 
     const fiatExchange = await this.fiatExchangeProvider.createExchange({
       address: exchange.exchangeAddress,
       amount: fiatEstimate.totalInFiat,
-      type: 'buy',
+      type: "buy",
       user: {
         email: user.email,
-        document: user.settings.kyc.data.document?.replace(/\D/g, '') || '',
-        phone: user.settings.contact.phone || '',
-        country: 'br',
+        document: user.settings.kyc.data.document?.replace(/\D/g, "") || "",
+        phone: user.settings.contact.phone || "",
+        country: "br",
         address: {
           street: user.settings.kyc.data.address.street as string,
           number: user.settings.kyc.data.address.number as string,
           zipCode: user.settings.kyc.data.address.zipCode as string,
         },
       },
-    })
-    console.log("Fiat exchange created for 'buy':", fiatExchange)
+    });
+    console.log("Fiat exchange created for 'buy':", fiatExchange);
 
     return this.transactionRepository.create({
       userId,
-      type: 'DEPOSIT',
-      fromCurrency: 'BRL',
-      toCurrency: 'ADA',
+      type: "DEPOSIT",
+      fromCurrency: "BRL",
+      toCurrency: "ADA",
       exchangeId: exchange.id,
       exchangeAddress: exchange.exchangeAddress,
       paymentId: fiatExchange.id,
@@ -140,6 +140,6 @@ export class CreateTransactionUseCase {
       fromAmount: amount,
       toAmount: exchange.toAmount,
       expiresAt: new Date(new Date().getTime() + EXPIRE_IN_15_MINUTES * 1000),
-    })
+    });
   }
 }
